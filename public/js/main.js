e2e4682bada5ffef42101fbd6319945520b39f36
zmcdaniel <zmcdaniel@gmail.com>
2016-08-26 10:36:16 -0700

    more styling; more attempts to get charts working

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,93 @@
-var ctx = document.getElementById("emotionsChart");
-var myChart = new Chart(ctx, {
+var ctxE = document.getElementById("emotionsChart");
+var eChart = new Chart(ctxE, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Anger", "Fear", "Disgust", "Joy", "Sadness"],
         datasets: [{
             label: '',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [0,0,0,0,0],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+
+var ctxL = document.getElementById("languageChart");
+var eChart = new Chart(ctxL, {
+    type: 'bar',
+    data: {
+        labels: ["Analytical", "Confident", "Tenative"],
+        datasets: [{
+            label: '',
+            data: [0,0,0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+var ctxS = document.getElementById("socialChart");
+var sChart = new Chart(ctxS, {
+    type: 'bar',
+    data: {
+        labels: ["Openness", "Conscientousness", "Extraversion", "Agreeableness", "Emotional Range"],
+        datasets: [{
+            label: '',
+            data: [0,0,0,0,0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1
         }]
